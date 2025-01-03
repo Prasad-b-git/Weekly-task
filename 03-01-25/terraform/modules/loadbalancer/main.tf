eaa4be407d62a9357dc88eb888c3b4da4495cf62
Prasad Bandagale <120239484+Prasad-b-git@users.noreply.github.com>
2025-01-03 12:29:31 +0530

    Create main.tf

---
+++ 03-01-25/terraform/modules/loadbalancer/main.tf
@@ -0,0 +1,55 @@
+resource "aws_lb" "alb" {
+  name               = "${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.public_sg_id]
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name        = "${var.environment}-alb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "${var.environment}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher            = "200"
+    path               = "/"
+    port               = "traffic-port"
+    protocol           = "HTTP"
+    timeout            = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name        = "${var.environment}-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = var.instance_id
+  port             = 80
+}
