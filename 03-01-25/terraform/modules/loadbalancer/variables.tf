0a8f0fe35cae9eaa0fef2f492fe15e80794f4992
Prasad Bandagale <120239484+Prasad-b-git@users.noreply.github.com>
2025-01-03 12:28:41 +0530

    Update variables.tf

--- 03-01-25/terraform/modules/loadbalancer/variables.tf
+++ 03-01-25/terraform/modules/loadbalancer/variables.tf
@@ -1,11 +1,3 @@
-output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
-
-output "target_group_arn" {
-  value = aws_lb_target_group.target_group.arn
-}
-root@terraform:/home/azureuser/terraform/modules/loadbalancer# cat variables.tf
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
