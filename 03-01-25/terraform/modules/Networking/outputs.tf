a123250b4b70567e0eb3e7951415bc2681ba5c54
Prasad Bandagale <120239484+Prasad-b-git@users.noreply.github.com>
2025-01-03 12:22:12 +0530

    Create outputs.tf

---
+++ 03-01-25/terraform/modules/Networking/outputs.tf
@@ -0,0 +1,45 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+root@terraform:/home/azureuser/terraform/modules/networking# cat outputs.tf 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnets[*].id
+}
+
+output "nat_gateway_ids" {
+  value = aws_nat_gateway.main[*].id
+}
