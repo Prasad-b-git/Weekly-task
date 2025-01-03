73ad06d837b6d3a672fbb33c5bd3188314617965
Prasad Bandagale <120239484+Prasad-b-git@users.noreply.github.com>
2025-01-03 12:24:09 +0530

    Create variable.tf

---
+++ 03-01-25/terraform/modules/compute/variable.tf
@@ -0,0 +1,40 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "public_sg_id" {
+  description = "Public security group ID"
+  type        = string
+}
+
+variable "private_sg_id" {
+  description = "Private security group ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
